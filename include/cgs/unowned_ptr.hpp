814e7bd9f13aae1930beb98e2098b54f0c234c7d
Cory Sherman <coryshrmn@gmail.com>
2017-10-19 13:30:22 -0700

    improve unowned_ptr documentation
    Also removed unowned_ptr<T>::operator=(T* p).
    It is automatic, since T* implicitely converts to unowned_ptr.

--- include/cgs/unowned_ptr.hpp
+++ include/cgs/unowned_ptr.hpp
@@ -36,54 +36,72 @@ public:
         : _p(p)
     { }
 
-    constexpr unowned_ptr& operator=(T* p) noexcept
-    {
-        _p = p;
-        return *this;
-    }
-
+    /**
+     * @brief Use the pointer, must not be null.
+     */
     constexpr T& operator*() const
     {
         cgs_assert(_p);
         return *_p;
     }
 
+    /**
+     * @brief Use the pointer, must not be null.
+     */
     constexpr T* operator->() const
     {
         cgs_assert(_p);
         return _p;
     }
 
-    constexpr T* or_null() const noexcept
-    {
-        return _p;
-    }
-
-    constexpr operator bool() const noexcept
-    {
-        return static_cast<bool>(_p);
-    }
-
-    constexpr bool operator!() const noexcept
-    {
-        return !_p;
-    }
-
-    constexpr bool operator==(const unowned_ptr& rhs) const noexcept
-    {
-        return _p == rhs._p;
-    }
-
-    constexpr bool operator!=(const unowned_ptr& rhs) const noexcept
-    {
-        return _p != rhs._p;
-    }
-
+    /**
+     * @brief Use the pointer to call a function, must not be null.
+     */
     template <typename... Args>
     constexpr auto operator()(Args&&... args) const
     {
         cgs_assert(_p);
         return _p(std::forward<Args>(args)...);
+    }
+
+    /**
+     * @brief Explicitly get the pointer, even if it is null.
+     */
+    constexpr T* or_null() const noexcept
+    {
+        return _p;
+    }
+
+    /**
+     * @brief Is this not null?
+     */
+    constexpr operator bool() const noexcept
+    {
+        return static_cast<bool>(_p);
+    }
+
+    /**
+     * @brief Is this null?
+     */
+    constexpr bool operator!() const noexcept
+    {
+        return !_p;
+    }
+
+    /**
+     * @brief Compares pointers, not values.
+     */
+    constexpr bool operator==(const unowned_ptr& rhs) const noexcept
+    {
+        return _p == rhs._p;
+    }
+
+    /**
+     * @brief Compares pointers, not values.
+     */
+    constexpr bool operator!=(const unowned_ptr& rhs) const noexcept
+    {
+        return _p != rhs._p;
     }
 };
 
