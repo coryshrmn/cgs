f6575e9b2a1c584f84f81ce3eb05442bc7252b9b
Cory Sherman <coryshrmn@gmail.com>
2017-11-02 00:11:17 -0700

    proof of concept for better simd_vec4 than glm's.

---
+++ include/cgs/simd/vec4.hpp
@@ -0,0 +1,24 @@
+/*
+   Copyright 2017 Cory Sherman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+#ifndef CGS_SIMD_VEC4_HPP
+#define CGS_SIMD_VEC4_HPP
+
+/*
+Compiler Explorer, showing we can pass more efficiently than glm's simd_vec4 (glm::aligned_highp_vec4).
+http://localhost:10240/#z:OYLghAFBqd5QCxAYwPYBMCmBRdBLAF1QCcAaPECAKxAEZSBnVAV2OUxAHIBSAJgGY8AO2QAbZlgDU3fgGFgogLYB6BYoB0CAA5aZ2bgAYAgn0EjxUmfKXLMADwKadewyeOnhYiZmly7ADgA2YQJiYU0Xd2MxAEMGBkkAN0xkABYAfWRmBiJFV24AdgAhVy0wxJiCDldJWsl09MVaXn969EqYmRLjOuljLWYAI1E8ZBB8nrrktMzs3IgASl8AEUksADMY5lECLomjXuUAKjxFLRHkQiPlJJSMrJzURQgGppa1joWa3rqQNo7pMV2gROgVlt9aoUioDwVEDlM7rNHs91qJUJVJHZSJJUeiCJIAJ7Y3EYgBexLRGIA7l9Jj9JH90sCYoCiljCdjyZIqYVYfDIcUYftetN7nMnpJUFpMMRKiQ%2BEUIKKkblJMQEAwlmghDkIay9T9iJgCKwhPVFIp0jF0Oh0loGC9mdj1Qx1EzPns6UL3GDPW4jPYqsQzWoQCBRfTIwiZjFsVHlYM/YGZSGlGGYiNgEJMLaEHhgNp0hHbjHM9nbbGSxkM/ny%2BlE/xugGHCmqyqJVGfsqHrkrXHu%2BLLQ2m8ng/VGs1Wp3O29/H2Z5P637XFUzqJKj4rAQCdKhDFFD4ACqRIyJVB4dCSbUMZgHospCCHyQxL6N/Zni/Pm3pNSLfLFPVr1vTB72QJVERZBU20GV8m15d9z0va1bTUdIGFOdA/x9JteiAu9pnA0taxzK1pF4aFlRrLMSJg5dfThD8kO/HsnjQjCsJMACvTwkCCIHZFSKg/je1ot8fT5VxGK/W1Zw4qFANQHVgNAl4LStb97QgWd520mDYP/PlOAWUhRC4ABWThSCELgDEs1AuFkBUoKYVh2DI/haEsggbKM4yAGsQDMgx1DM2gDFScL%2BAATgKfh%2BAMXgTK4VJLOszhbNIezOEshgQAMUhvPSozSDgWAkDQM48FEGVyEoCqtCqmUQGUbJiGUQZhGUWIhGAJzyNIdYqqDXKIEGHzSA6vdiAJLhPNICqDyEAgAHkhFEGaitILBFBiHrqvG/AjWQAg8GSXLNvsFJmCqWbLJCTBTM20JTh84yRkGXLIGMqUTsU86AFocnQGRlmQPqiloApJH%2B5beBylg2A4Wg3vM1LxqylqGDayauvXHrwckCBcEIEh3PoSRZCeBrquIMmFi817/JAVIovUVJ/CiszOcCBLAkCKL/FoQIks4FKrPRrgcrygrGZFuHxc2rKGaKhZjOSYh0MU5mgA%3D%3D%3D
+*/
+
+#endif // CGS_SIMD_VEC4_HPP
